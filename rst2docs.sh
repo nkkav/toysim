6d56d6582d034ade2e1dc1fc353a6e98fa7858c0
Nikolaos Kavvadias <kavvadias@silexica.com>
2016-08-22 08:47:16 +0200

    Remove affiliation

--- rst2docs.sh
+++ rst2docs.sh
@@ -4,7 +4,7 @@
 #DOCUTILS_PATH="/cygdrive/c/docutils-0.11/tools"
 #DOCUTILS_PATH="/usr/local/bin/docutils/tools"
 #DOCUTILS_PATH="/c/docutils-0.11/tools"
-DOCUTILS_PATH=/usr/bin
+DOCUTILS_PATH="/usr/bin"
 
 ${DOCUTILS_PATH}/rst2html <$1.rst >$1.html
 ${DOCUTILS_PATH}/rst2latex <$1.rst >$1.tex
